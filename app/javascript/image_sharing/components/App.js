c80aca83c95e6cbccb53dd87e2a74b4d3173fcd3
Aradhya Gupta <aradhya.gupta@appfolio.com>
2019-01-30 11:41:23 -0800

    ag added the feedback form

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { inject } from 'mobx-react';
 import Header from './Header';
 import Footer from './Footer';
+import FeedbackForm from "./FeedbackForm";
+
 class App extends Component {
   render() {
     return (
       <div>
         <Header title={'Tell us what you think'} />
+        <FeedbackForm />
         <Footer />
       </div>
     )
